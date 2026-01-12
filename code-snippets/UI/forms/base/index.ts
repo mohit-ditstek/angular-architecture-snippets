8900c8d352f126b583473e95f8ba71fb5f5ca56c
Mohit <mohit.dhiman@ditstek.com>
2026-01-12 11:59:37 +0530

    Code Snippets

--- code-snippets/UI/forms/base/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './base-value-accessor';
-export * from './base-form-control';
-export * from './base-field.control';
-export * from './base-auto-complete';
-export * from './base-user-field-auto-complete';
--- code-snippets/UI/forms/form-controls/control-wrapper/control-wrapper.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'app-control-wrapper',
-  templateUrl: './control-wrapper.component.html',
-})
-export class ControlWrapperComponent {
-  @Input() label: string = '';
-}
